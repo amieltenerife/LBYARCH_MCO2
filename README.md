a21ce426c0e13d1ce1b831b8858879de6865524f
eli <amiel_elijah_tenerife@dlsu.edu.ph>
2024-11-28 21:59:49 +0800

    Updated Code to take in User Input

--- README.md
+++ README.md
@@ -5,18 +5,17 @@ Google Drive Folder for Vector Elements:
 https://drive.google.com/drive/folders/1ka8_ms5F_SCvAwsTGNa2eAoQQ79deU-4?usp=sharing
 
 Screenshot of the Result After Running the C Reference Program:
-![C Program Result](C_Result.png)
+![Reference_Result](Reference.png)
 
 Screenshot of the Result After Running the Kernel:
-![Kernel Program Result](Kernel_Result.png)
+![Kernel_Result](Kernel.png)
 
 Screenshot of the Results of the C Reference Program and the Kernel Program:
-![C Program and Kernel Program Result](C_Kernel_Result.png)
+![Reference_Kernel_Result](Reference_Kernel.png)
 
-Average Execution Times:<br/>
-For 2^20 Double-Precision Floating Point Values - 0.00095 seconds<br/>
-For 2^24 Double-Precision Floating Point Values - 0.0138 seconds<br/>
-For 2^29 Double-Precision Floating Point Values - 0.4664 seconds<br/>
+Average Execution Time For Reference Program: 0.000000 seconds<br>
+Average EXecution Time For Kernel Program: 0.000000 seconds<br>
+<br/>
 
 Analysis: <br/>
-After getting the average execution time of each vector, it can be said that the length of the vector and the average execution time are dependent of each other. As the length of each vector increases, the execution time also increases. This is because a vector with a larger length increases the size of the bin file and more memory will be have to be allocated for the read and write operations, resulting to longer execution times.
+After getting the average execution time of each vector, it can be said that the length of the vector and the average execution time are dependent of each other. As the length of each vector increases, the execution time also increases. This is because a vector with a larger size increases the size of the memory needed to be allocated for the read and write operations, resulting in longer execution times.
--- test_MCO2.c
+++ test_MCO2.c
@@ -2,92 +2,11 @@
 #include<stdlib.h>
 #include<time.h>
 
-/*
-void read_vector(const char* filename, double* vector, size_t size) {
-	FILE* file = fopen(filename, "rb");
-	if (!file) {
-		perror("Failed to open file");
-		exit(EXIT_FAILURE);
-	}
-	
-	fread(vector, sizeof(double), size, file);
-	fclose(file);
-}
-*/
-
 int main() {
-	/*
-	size_t size20 = 1 << 20;
-	size_t size24 = 1 << 24;
-	size_t size29 = 1 << 29;
-	double* vector1 = malloc(size20 * sizeof(double));
-	double* vector2 = malloc(size20 * sizeof(double));
-	double sum = 0;
-	int i;
-	
-	if (!vector1 || !vector2) {
-		perror("Failed to allocate memory");
-		return EXIT_FAILURE;
-	}
-	
-	read_vector("vector1.bin", vector1, size20);
-	read_vector("vector2.bin", vector2, size20);
-	
-	for (i = 0; i < size20; i++) {
-		sum += vector1[i] * vector2[i];
-	}
-	
-	printf("sdot (20): %.2f\n", sum);
-	
-	free(vector1);
-	free(vector2);
-
-	vector1 = malloc(size24 * sizeof(double));
-	vector2 = malloc(size24 * sizeof(double));
-	sum = 0;
-
-	if (!vector1 || !vector2) {
-		perror("Failed to allocate memory");
-		return EXIT_FAILURE;
-	}
-	
-	read_vector("vector1_large.bin", vector1, size24);
-	read_vector("vector2_large.bin", vector2, size24);
-	
-	for (i = 0; i < size24; i++) {
-		sum += vector1[i] * vector2[i];
-	}
-	
-	printf("sdot (24): %.2f\n", sum);
-	
-	free(vector1);
-	free(vector2);
-
-	vector1 = malloc(size29 * sizeof(double));
-	vector2 = malloc(size29 * sizeof(double));
-	sum = 0;
-
-	if (!vector1 || !vector2) {
-		perror("Failed to allocate memory");
-		return EXIT_FAILURE;
-	}
-
-	read_vector("vector1_largest.bin", vector1, size29);
-	read_vector("vector2_largest.bin", vector2, size29);
-
-	for (i = 0; i < size29; i++) {
-		sum += vector1[i] * vector2[i];
-	}
-
-	printf("sdot (29): %.2f", sum);
-
-	free(vector1);
-	free(vector2);
-	*/
-
 	int n = 5;
-	double sum = 0;
 	int i;
+
+	double sum = 0;
 	double* A = (double*)malloc(n * sizeof(double));
 	double* B = (double*)malloc(n * sizeof(double));
 
@@ -116,6 +35,6 @@ int main() {
 
 	free(A);
 	free(B);
-	
+
 	return 0;
 }
